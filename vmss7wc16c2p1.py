999e868daf1874b17294099d9257d3c5e1a807a5
chengary88 <44847256+chengary88@users.noreply.github.com>
2019-02-16 14:46:25 -0500

    Create vmss7wc16c2p1.py

---
+++ vmss7wc16c2p1.py
@@ -0,0 +1,13 @@
+g1 = "G"
+period = "."
+a = int(input())
+for i in range(a):
+  print(g1*5*a)
+for i in range(a):
+  print(g1*a+period*4*a)
+for i in range(a):
+  print(g1*a+period*2*a+g1*2*a)
+for i in range(a):
+  print(g1*a+period*3*a+g1*a)
+for i in range(a):
+  print(g1*5*a)
